55f57ac6b0418a46edd082c264742fa4de64c791
angel <angelblas17@gmail.com>
2018-10-09 17:37:03 -0700

    Added CSS and Experience

--- js/app.js
+++ js/app.js
@@ -10,15 +10,15 @@ alert('Glad you stopped by, ' + username)
 var answerOne = prompt('Do I like Halloween?').toLowerCase()
 
 if (answerOne === 'yes' || answerOne === 'y' || answerOne === 'yup') {
-    // Tell them
-    alert('You know it');
-    // console.log the result
-console.log('The user answered correctly')
+  // Tell them
+  alert('You know it');
+  // console.log the result
+  console.log('The user answered correctly')
 
 } else {answerOne=== 'no' || answerOne === 'n' || answerOne === 'nope';
-    // Tell them
-    alert('BOOOO! That is not right!'); 
-    // console.log the result
+  // Tell them
+  alert('BOOOO! That is not right!');
+  // console.log the result
   console.log('The user answered incorrectly');
 }
 
@@ -26,51 +26,51 @@ var answerTwo = prompt('Do I has all the bacons?').toLowerCase()
 
 if (answerTwo === 'yes' || answerTwo === 'y' || answerTwo === 'yup') {
 
-    alert('Shhhhh tell know one');
+  alert('Shhhhh tell know one');
 
-    console.log('The user answered correctly');
+  console.log('The user answered correctly');
 
 } else {answerTwo === 'no' || answerTwo === 'n' || answerTwo === 'nope';
 
-alert('You underestimate me FOOL!');
+  alert('You underestimate me FOOL!');
 
-console.log('The user answered incorrectly');
+  console.log('The user answered incorrectly');
 }
 
 var answerThree = prompt('Do I like cake?').toLowerCase()
 
 if (answerThree === 'yes' || answerThree === 'y' || answerThree === 'yup') {
 
-    alert('Ew gross');
-    console.log('The user answered incorrectly');
+  alert('Ew gross');
+  console.log('The user answered incorrectly');
 
 } else {answerThree === 'no' || answerThree === 'n' || answerThree === 'nope';
 
-alert('Brownies are better');
-console.log('the user answered correctly');
+  alert('Brownies are better');
+  console.log('the user answered correctly');
 }
 
 var answerFour = prompt('Do I love Boston Terriers?').toLowerCase()
 
 if (answerFour === 'yes' || answerFour === 'y' || answerFour === 'yup') {
 
-    alert('Darn tootin! That is why I have two!');
-    console.log('the user answered correctly');
+  alert('Darn tootin! That is why I have two!');
+  console.log('the user answered correctly');
 } else {answerFour === 'no' || answerFour === 'n' || answerFour === 'nope';
 
-alert('How dare you insult my two fur babies!');
-console.log('the user answered incorrectly') 
+  alert('How dare you insult my two fur babies!');
+  console.log('the user answered incorrectly');
 }
 
 
 var answerFive = prompt('Is my favorite show All My Childeren?').toLowerCase()
 
-if (answerFive === 'yes' || answerFive === 'y' || answerFive === "yup") {
+if (answerFive === 'yes' || answerFive === 'y' || answerFive === 'yup') {
 
-    alert('DOH!');
-    console.log('the user answered incorrectly');
+  alert('DOH!');
+  console.log('the user answered incorrectly');
 } else {answerFive === 'no' || answerFive === 'n' || answerFive === 'nope';
 
-alert('WOOHOO!');
-console.log('the user answered correctly')
+  alert('WOOHOO!');
+  console.log('the user answered correctly')
 }
